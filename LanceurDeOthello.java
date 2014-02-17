15598dc89665bf741b1a3703565fed9ca4f28b73
durotm <durotm@PC-B102-01.iut-valence.fr>
2014-02-17 16:26:53 +0100

    Création du lanceur du jeu

---
+++ LanceurDeOthello.java
@@ -0,0 +1,14 @@
+
+public class LanceurDeOthello {
+
+	public static void main(Strings[] args){
+		
+		System.out.println("Création d'une partie de Othello");
+		partieOthello = new Partie();
+		System.out.println("Demarrage de la partie");
+		partieDeOthello.jouer();
+		System.out.println("Fin de la partie");
+	}
+	
+	
+}
